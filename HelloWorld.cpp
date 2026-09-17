c1573ade401372d3c611b8825f048bd48d8736ee
Fatima Khiari <hv3974@wayne.edu>
2026-09-16 23:31:55 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Fatima Khiari, hv3974@wayne.edu 
 #include<iostream>  
   
 using namespace std; 
